603332566c7350f21196179f84a0763484f98e8f
Earle-Poole <eepoole4@gmail.com>
2018-11-11 22:35:02 -0600

    v3 data.outputs/babel arguments

--- src/App.js
+++ src/App.js
@@ -54,8 +54,8 @@ class App extends Component {
     }})
   }
 
-   calculateFaceLocation = function(data) {
-    const clarifaiFace = _this.data.outputs[0].data.regions[0].region_info.bounding_box;
+   calculateFaceLocation = function(response) {
+    const clarifaiFace = response.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width)
     const height = Number(image.height)
