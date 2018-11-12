940bdc687f2ad27ae6695dfb5e48260beeed7ee9
Earle-Poole <eepoole4@gmail.com>
2018-11-11 23:58:15 -0600

    bugs

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
   }
 
    calculateFaceLocation(data) {
-    let clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    let clarifaiFace = data.outputs.data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width)
     const height = Number(image.height)
